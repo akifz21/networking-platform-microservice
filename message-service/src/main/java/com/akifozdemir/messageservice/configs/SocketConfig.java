e480535ec7a828651cef4fbdfdca46c17d39ba95
akifz21 <akifz2199@gmail.com>
2023-12-22 17:32:56 +0300

    up: Socket Config

--- message-service/src/main/java/com/akifozdemir/messageservice/configs/SocketConfig.java
+++ message-service/src/main/java/com/akifozdemir/messageservice/configs/SocketConfig.java
@@ -15,7 +15,7 @@ public class SocketConfig  implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("*");
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
 }
